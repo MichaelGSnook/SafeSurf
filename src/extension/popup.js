cbc43122e71bd4e787fb4acc281eebb852b6c23e
snook <snook@iastate.edu>
2016-03-27 15:55:11 -0500

    Incremental update to popup

--- src/extension/popup.js
+++ src/extension/popup.js
@@ -1,39 +1,51 @@
+// 
+
 var a=0;
 function count() {
     a++;
     
     document.getElementById('demo').textContent = a;
-    document.getElementById('profiles').textContent = "HEHEHE";
+    var profiles = document.getElementById('profiles');
+    var div = document.getElementById('profiles-list');
+    // profiles.textContent = "HAHAHAHA";
 
-    $.get("06_2_simplestAjax.php?name=simanta", 
-        function(data,status) {
-           $("#myDiv").html(data);
+    $.ajax({	
+	    url: "http://fun.thomasmoll.co:5000/safesurf/users",
+	    data: null,
+	    success: loadUsers,
+	    dataType: "json"  
+	});
+}
 
-   });
+function loadUsers(response){
+	console.log(response['users'])
+	var profiles = document.getElementById('profiles-list');
+	
 
+	var name;
+	// var radioHtml = "";
+	// for (var i = 0; i < response['users'].length; i++) {
+	// 	var name = response['users'][i];
+	// 	var element = document.createElement("input");
+	// 	//Assign different attributes to the element.
+ //        element.setAttribute("type", "radio");
+ //        element.setAttribute("name", name);
+	    
+	//     // radioHtml += '<input type="radio" name="' + name + '"' + '/>';
+	//     profiles.appendChild(element);
+	//     // profiles.innerHTML.append(radioHtml);
+	// }​
+
+	for(name in response['users']){
+		var radioBtn = document.createElement("input");
+		//Assign different attributes to the element.
+	    radioBtn.setAttribute("type", "radio");
+	    radioBtn.setAttribute("name", name);
+	    
+	    // radioHtml += '<input type="radio" name="' + name + '"' + '/>';
+	    profiles.appendChild(radioBtn);
+	    profiles.appendChild(document.createElement("div"));
+	}
+	
 }
 document.getElementById('do-count').onclick = count;
-
-// GET THE DOM OBJECTS
-// var btn1 = $("#button1");
-// var btn2 = $("#button2"); // jquery is easier to use
-
-// ATTACH HANDLERS
-// First way - attach name of a function
-// btn2.click(toggleColor);
-
-
-// second way - attach ANONYMOUS handler 
-// (makes sense to not give names to one-time time use handlers)
-// console.log("About to attach handler");
-
-
-// function loadProfiles() {
-//   alert("Now going to make a GET ajax call - results will appear soon");
-//   $.get("06_2_simplestAjax.php?name=simanta", 
-//         function(data,status) {
-//            $("#myDiv").html(data);
-
-//    });
-// }
-// document.getElementById('button2').onclick = loadProfiles;
